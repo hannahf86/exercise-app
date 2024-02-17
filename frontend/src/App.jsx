f2b416f896356b732769d28a8494ea4599b08df7
hannahf86 <hannahfeehan.design@gmail.com>
2024-02-17 12:55:02 +0000

    frontend start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+// RRD
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={Home} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
