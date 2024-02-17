6655c2fa55ca066c0b68a79000d3d347a9289d75
hannahf86 <hannahfeehan.design@gmail.com>
2024-02-17 13:25:47 +0000

    workoutdetails component complete - eslint error resolved

---
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
