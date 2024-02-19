c348eea63b2f6642fc02655c1253575f182ab7e6
hannahf86 <hannahfeehan.design@gmail.com>
2024-02-19 14:47:56 +0000

    api address test

--- frontend/src/Components/WorkoutDetails.jsx
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -14,7 +14,8 @@ const WorkoutDetails = ({ workout }) => {
 
   const handleClick = async () => {
     const response = await fetch(
-      "https://exercise-app-beta.vercel.app/api/workouts" + workout._id,
+      "https://exercise-35750bmtz-hannah-feehans-projects.vercel.app/api/workouts" +
+        workout._id,
       {
         method: "DELETE",
       }
