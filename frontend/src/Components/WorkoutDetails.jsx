e0321bf3bb7d735ed09cd7e8f9092913c8621f3e
hannahf86 <hannahfeehan.design@gmail.com>
2024-02-19 18:48:38 +0000

    api route test

--- frontend/src/Components/WorkoutDetails.jsx
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -13,12 +13,9 @@ const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutContext();
 
   const handleClick = async () => {
-    const response = await fetch(
-      "https://exercise-app-beta.vercel.app/api/workouts" + workout._id,
-      {
-        method: "DELETE",
-      }
-    );
+    const response = await fetch("/api/workouts" + workout._id, {
+      method: "DELETE",
+    });
     const json = await response.json();
 
     if (response.ok) {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,9 +10,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch(
-        "https://exercise-app-beta.vercel.app/api/workouts"
-      );
+      const response = await fetch("/api/workouts");
 
       const json = await response.json();
 
