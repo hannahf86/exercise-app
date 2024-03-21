85294e70cf1dbc9735daa60b97a4eb3069726a6b
hannahf86 <hannahfeehan.design@gmail.com>
2024-03-21 13:49:14 +0000

    fetch problems resolved

--- frontend/src/Components/WorkoutDetails.jsx
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -13,9 +13,12 @@ const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutContext();
 
   const handleClick = async () => {
-    const response = await fetch("/api/workouts" + workout._id, {
-      method: "DELETE",
-    });
+    const response = await fetch(
+      "http://localhost:3000/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
     const json = await response.json();
 
     if (response.ok) {
